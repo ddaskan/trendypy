7ff38831f83bd3786ac096d4569ae473a55f06de
Dogan Askan <doganaskan@gmail.com>
2020-08-29 21:53:45 -0400

    added levenshtein_distance

--- trendypy/algos.py
+++ trendypy/algos.py
@@ -81,3 +81,47 @@ def fastdtw_distance(x, y, d=utils.distance_euclidean):
 
     '''
     return fastdtw(x, y, dist=d)[0]
+
+def levenshtein_distance(x, y):
+    """Levenshtein distance for string similarity.
+
+    Args:
+        x (str): input string 1
+        y (str): input string 2
+
+    Returns:
+        int: distance, 0 means strings are exactly same, upper limit is 
+            positive infinity
+
+    References:
+        https://en.wikipedia.org/wiki/Levenshtein_distance
+
+    Examples:
+        >>> levenshtein_distance('Apple', 'Apple')
+        0
+        >>> levenshtein_distance('Apple', 'apple')
+        1
+        >>> levenshtein_distance('Apple Inc.', 'apple inc')
+        3
+
+    """
+    m, n = len(x), len(y)
+    v0 = list(range(n + 1))
+    v1 = [None] * (n + 1)
+
+    for i in range(m):
+        v1[0] = i + 1
+
+        for j in range(n):
+            deletion_cost = v0[j + 1] + 1
+            insertion_cost = v1[j] + 1
+            if x[i] == y[j]:
+                substitution_cost = v0[j]
+            else:
+                substitution_cost = v0[j] + 1
+
+            v1[j + 1] = min(deletion_cost, insertion_cost, substitution_cost)
+
+        v0, v1 = v1, v0
+
+    return v0[n]
