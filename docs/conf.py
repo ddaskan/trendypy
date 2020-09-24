2840887d682b8dcb2a120b961e03e28e5920367b
Dogan Askan <doganaskan@gmail.com>
2020-09-24 18:51:32 -0400

    renamed version

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Dogan Askan'
 author = 'Dogan Askan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.0'
+release = '0.2.1'
 
 
 # -- General configuration ---------------------------------------------------
