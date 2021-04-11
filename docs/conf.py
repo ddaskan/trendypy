3a85212e292956379b87a75eeb7998ba9b60df81
Dogan Askan <doganaskan@gmail.com>
2021-04-11 14:56:47 -0400

    version bump

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2020, Dogan Askan'
 author = 'Dogan Askan'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.2.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
 
 setuptools.setup(
     name="trendypy",
-    version="0.2.1",
+    version="0.2.2",
     author="Dogan Askan",
     author_email="doganaskan@gmail.com",
     description="A package for trend line clustering.",
